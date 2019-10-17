194926cf5ce93809a7e1b75fc269078e11176b0c
pandeyKaviraj <kaviraj.pandey11@gmail.com>
2019-10-17 16:22:16 +0530

    Initial Commit

---
+++ tempConverter/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  tempConverter
+//
+//  Created by KAVIRAJ PANDEY on 17/10/19.
+//  Copyright © 2019 KAVIRAJ PANDEY. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
