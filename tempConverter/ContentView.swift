1335a01171b1293984d800254c9c4647dcc33f10
pandeyKaviraj <kaviraj.pandey11@gmail.com>
2019-10-17 16:54:52 +0530

    Segmented Picker feature added

--- tempConverter/ContentView.swift
+++ tempConverter/ContentView.swift
@@ -9,10 +9,49 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var tempInput = ""
+    @State private var firstUnits = ["℃", "℉", "K"]
+    @State private var firstDefaultUnits = "℃"
     var body: some View {
-        Text("Hello World")
+        NavigationView {
+            Form {
+                
+                //First Unit of work done!
+                Section(header: Text("Choose One Of Units")) {
+                    Picker("Choose One Unit", selection: $firstDefaultUnits) {
+                        ForEach (0 ..< firstUnits.count) {
+                            Text("\(self.firstUnits[$0])")
+                        }
+                    }
+                .pickerStyle(SegmentedPickerStyle())//Picker modifier
+                }
+                
+                
+                //Textfield for Entering value
+                Section {
+                    TextField("Enter Value For Temp Conversion", text: $tempInput)
+                        .keyboardType(.decimalPad)
+                }
+                
+                
+            }//Form Closes
+        .navigationBarTitle("Temperature Converter!")
+        }
+        
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+//MARK- Preview Section
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
